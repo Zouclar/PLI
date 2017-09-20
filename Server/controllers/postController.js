6a1a3ebdc1e9edc81bf2681f8e73c6e01ddad7d6
bassez <bassez_f@etna-alternance.net>
2017-09-20 17:05:21 +0200

    Controller new Orm v2

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -3,14 +3,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 
-
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'PLI',
-    password : 'pli',
-    port     : '8888',
-    database : 'PLI'
-});
+var database = require('../config/config.js');
 
 class PostController {
 
@@ -21,49 +14,40 @@ class PostController {
             console.log(req.body);
             console.log(req.files);
             console.log(req.fields);
-
-            connection.connect();
-            for (var item of req.fields.json){
-
-                connection.query("INSERT INTO `posts`(`title`, `coordinate`, `description`, `link`, `date_pub`, `number_like`, `number_dislike`, `picture`) VALUES ("+item.title+","+item.coordinate+","+item.description+","+item.link+","+item.date_pub+","+item.number_like+","+item.number_dislike+","+req.files.file.path, function (error, results, fields) {
-                    if (error) throw error;
-                    console.log(results[0].solution);
+            database('localhost', 'PLI', function(err, db) {
+                if (err) throw err;
+                console.log("CONNEXION GOOD")
+                db.models.post.create({title: 'FirstPosition'}, function(err, rows) {
 
                 });
-            }
-            /connection.end();
+            });
+
             res.status(200).json("OK")
         }
     }
     static read (req, res, next) {
-        connection.connect();
 
-        connection.query("SELECT * FROM posts WHERE id='" +req.params.id+ "'", function (error, results, fields) {
-            if (error) throw error;
-            console.log('The solution is: ', results[0].solution);
-            results[0].solution.link
+        database('localhost', 'PLI', function(err, db) {
+            if (err) throw err;
 
+            db.models.post.find({foo: 'bar'}, function(err, rows) {
+
+            });
         });
-
-        connection.end();
         res.status(200).json()
     }
     // static upload (req, res, next) {
     //     res.status(200).json()
     // }
     static readAll (req, res, next) {
-        connection.connect();
+        database('localhost', 'PLI', function(err, db) {
+            if (err) throw err;
 
-        connection.query("SELECT * FROM posts", function (error, results, fields) {
-            if (error) throw error;
-            console.log('The solution is: ', results[0].solution);
-            for(var item of res){
+            // for(var item of )
+                db.models.post.find({foo: 'bar'}, function(err, rows) {
 
-            }
-                //get file
+                });
         });
-
-        connection.end();
         res.status(200).json()
     }
 
